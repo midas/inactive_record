194568ae35c60c6d17036cf25b026c5c0606d55b
Jason Harrelson <jason@lookforwardenterprises.com>
2010-07-19 11:48:46 -0500

    Removed attr_human_name method and accompanying methods in favor of I18n.translate.

--- lib/inactive_record/base.rb
+++ lib/inactive_record/base.rb
@@ -140,26 +140,11 @@ module InactiveRecord
     def self.human_name( options={} )
       Base.human_attribute_name(@name)
     end
-    
-    # Allows alternate humanized versions of attributes to be set.  For example, an attribute such as 'num_employees' would be
-    # converted to 'Num employees' normally using <tt>human_attribute_name</tt>.  More descriptive text can be set. Example:
-    #   attr_human_name 'num_employees' => 'Number of employees'
-    def self.attr_human_name(attributes) # :nodoc:
-      attributes.stringify_keys!
-      write_inheritable_hash("attr_human_name", attributes || {})
-
-      # assign the current class to each column that is being assigned a new human attribute name
-      #self.columns.reject{|c| !attributes.has_key?(c.name)}.each{|c| c.parent_record_class = self}
-    end
-    
-    def self.human_name_attributes # :nodoc:
-      read_inheritable_attribute("attr_human_name")
-    end
 
     # Transforms attribute key names into a more humane format, such as "First name" instead of "first_name". Example:
     #   Person.human_attribute_name("first_name") # => "First name"
     def self.human_attribute_name(attribute_key_name) #:nodoc:
-      (read_inheritable_attribute("attr_human_name") || {})[attribute_key_name.to_s] || ""
+      I18n.translate( "inactive_record.attributes.#{self.name.underscore.gsub( /\//, '.' )}.#{attribute_key_name.to_s}")
     end
 
     protected
